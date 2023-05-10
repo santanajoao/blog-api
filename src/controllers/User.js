ecddba595491cdf7b0264a2b3f4b2882470bd9e7
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-10 19:05:06 -0300

    cria controller de criação de usuário

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,4 +1,4 @@
-const { OK } = require('../constants/statusCodes');
+const { OK, CREATED } = require('../constants/statusCodes');
 const { UserService } = require('../services');
 const mapTypeToStatus = require('../utils/mapTypeToStatus');
 
@@ -12,6 +12,15 @@ const handlePostLogin = async (req, res) => {
   res.status(OK).json({ token: message });
 };
 
+const handlePostUser = async (req, res) => {
+  const { type, message } = await UserService.createUser(req.body);
+  if (type) {
+    return res.status(mapTypeToStatus(type)).json({ message });
+  }
+  res.status(CREATED).json({ token: message });
+};
+
 module.exports = {
   handlePostLogin,
+  handlePostUser,
 };
