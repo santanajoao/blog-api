e5a4b3a67787de68e2192ee9f720496a0ef7ddcb
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-10 15:28:16 -0300

    cria migration da tabela categories

---
+++ src/migrations/20230510182542-create-categories-table.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('categories');
+  }
+};
