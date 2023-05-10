efc0e627aef17f4880f9894cccb12f4c61aa178b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-10 15:21:20 -0300

    cria migration da tabela users

---
+++ src/migrations/20230510180358-create-user-table.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      display_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: Sequelize.STRING,
+    });
+  },
+
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
