0cb541219c0ec15c02cd17d6c42def2a131087a6
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-11 09:31:23 -0300

    cria o model da tabela de categorias

---
+++ src/models/Category.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return Category;
+};
