f7e31f88b9a86c00b17c183f6a511929077c4872
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-11 10:49:47 -0300

    cria o model BlogPost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * 
+ * @param {import('DataTypes').DataTypes} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    content: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    published: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW(),
+    },
+    updated: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW(),
+    }
+  }, {
+    tableName: 'blog_posts',
+    timestamps: true,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'users'
+    });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -31,5 +31,11 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blog_posts'
+    });
+  };
+
   return User;
 };
