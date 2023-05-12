ac75e819455543b8b9f7fafb3c5d6e230c7dbb5b
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-12 07:54:12 -0300

    remove código desnecessário

--- src/controllers/BlogPost.js
+++ src/controllers/BlogPost.js
@@ -63,7 +63,6 @@ const handleDeletePost = async (req, res) => {
 const handleGetPostSearch = async (req, res) => {
   const token = req.get('Authorization');
   const { q } = req.query;
-  console.log('>>>>> aqui <<<<<<');
 
   const { type, message } = await BlogPostService.searchPost(token, q);
   if (type) {
