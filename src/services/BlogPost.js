fc8499254a657e4c3b189c1255ea1fb28f47a142
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-11 22:37:31 -0300

    cria service que apaga um post

--- src/services/BlogPost.js
+++ src/services/BlogPost.js
@@ -1,18 +1,19 @@
 const { Op } = require('sequelize');
 const { BlogPost, PostCategory, User, Category } = require('../models');
 const validations = require('./validations');
+const { POST_NOT_FOUND } = require('../constants/errorTypes');
 
 const createPost = async ({
   authenticationToken, title, content, categoryIds,
 }) => {
-  const authError = validations.validateAuthToken(authenticationToken);
-  if (authError.type) return authError;
+  const tokenError = validations.validateAuthToken(authenticationToken);
+  if (tokenError.type) return tokenError;
 
   const categoryError = await validations.validateCategoryIds(categoryIds);
   if (categoryError.type) return categoryError;
   
   const user = await User.findOne({
-    where: { email: { [Op.eq]: authError.message.email } },
+    where: { email: { [Op.eq]: tokenError.message.email } },
   });
   const newPost = await BlogPost.create({ title, content, userId: user.id });
 
@@ -55,7 +56,7 @@ const findPostById = async (authorizationToken, postId) => {
     ],
   });
   if (!post) {
-    return { type: 'POST_NOT_FOUND', message: 'Post does not exist' };
+    return { type: POST_NOT_FOUND, message: 'Post does not exist' };
   }
 
   return { type: null, message: post };
@@ -77,9 +78,28 @@ const editPost = async ({ authorizationToken, postId, title, content }) => {
   return findPostById(authorizationToken, postId);
 };
 
+const deletePost = async (authorizationToken, postId) => {
+  const tokenError = validations.validateAuthToken(authorizationToken);
+  if (tokenError.type) return tokenError;
+
+  const permissionError = await validations
+    .validateUser(authorizationToken, postId);
+  if (permissionError.type) return permissionError;
+
+  const deletedPosts = BlogPost.destroy({
+    where: { id: postId },
+  });
+  if (deletedPosts === 0) {
+    return { type: POST_NOT_FOUND, message: 'Post does not exist' };
+  }
+
+  return { type: null, message: '' };
+};
+
 module.exports = {
   createPost,
   findAllPosts,
   findPostById,
   editPost,
+  deletePost,
 };
